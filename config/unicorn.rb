ea986462ab5f44ba55841a5af19d39f6dda6e246
schneems <richard.schneeman@gmail.com>
2013-05-19 17:27:14 -0500

    cleanup

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,11 +12,6 @@ before_fork do |server, worker|
     Rails.logger.info('Disconnected from ActiveRecord')
   end
 
-  # If you are using Redis but not Resque, change this
-  if defined?(Resque)
-    Resque.redis.quit
-    Rails.logger.info('Disconnected from Redis')
-  end
 end
 
 after_fork do |server, worker|
@@ -26,11 +21,4 @@ after_fork do |server, worker|
     Rails.logger.info('Connected to ActiveRecord')
   end
 
-  # If you are using Redis but not Resque, change this
-  if defined?(Resque)
-    ENV["REDISTOGO_URL"] ||= "redis://127.0.0.1:6379"
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    Rails.logger.info('Connected to Redis')
-  end
 end
